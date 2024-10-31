948474a9c89c42ba63b1ab6559449db327ed6d41
Sei Takahasi <s23C1082TL@s.chibakoudai.jp>
2024-10-31 13:03:05 +0900

    -xvをセット

--- test.bash
+++ test.bash
@@ -4,7 +4,6 @@
 ng () {
 	echo ${1}行目が違うよ
 	res=1
-
 }
 
 res=0
@@ -12,5 +11,13 @@ res=0
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
 
-[ "${res}" = 0 ] && echo OK
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]	  || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]	  || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+[ "$res" = 0 ] && echo OK
 exit $res
