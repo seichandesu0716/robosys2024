0932fd544b3f8aaa162b255e70a29c86b4c2a5be
Sei Takahasi <s23C1082TL@s.chibakoudai.jp>
2024-10-31 12:47:13 +0900

    テスト項目の追加

--- test.bash
+++ test.bash
@@ -9,14 +9,15 @@ ng () {
 res=0
 
 out=$(seq 5 | ./plus)
-++ ./plus
-+ out=15
 [ "${out}" = 15 ] || ng "$LINENO"
-+ '[' 15 = 15 ']'
 
-[ "${res}" = 0 ] && echo OK
-+ '[' 0 = 0 ']'
-+ echo OK
-OK
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]	  || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]	  || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+[ "$res" = 0 ] && echo OK
 exit $res
-+ exit 0
