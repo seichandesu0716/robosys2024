ac9116c2e41efee7775613460613f8b0b183dd5e
Sei Takahasi <s23C1082TL@s.chibakoudai.jp>
2024-10-31 12:38:24 +0900

    動作確認

--- test.bash
+++ test.bash
@@ -1,13 +1,22 @@
-#!/bin/bash
-
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2024 Sei Takahashi <seitaka_0716_poke@yahoo.com
+# SPDX-License-Identifier: BSD-3-Clause
 ng () {
 	echo ${1}行目が違うよ
 	res=1
 }
 
 res=0
-a=山田
-[ "$a" = 上田 ] || ng "$LINENO"
-[ "$a" = 山田 ] || ng "$LINENO"
 
+out=$(seq 5 | ./plus)
+++ ./plus
++ out=15
+[ "${out}" = 15 ] || ng "$LINENO"
++ '[' 15 = 15 ']'
+
+[ "${res}" = 0 ] && echo OK
++ '[' 0 = 0 ']'
++ echo OK
+OK
 exit $res
++ exit 0
